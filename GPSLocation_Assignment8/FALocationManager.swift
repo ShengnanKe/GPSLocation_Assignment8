28e674c9a83bf2fd906a8a3e0ca81e886e8c7b83
KknanxX <72762570+KknanxX@users.noreply.github.com>
2024-06-10 23:05:48 -0700

    with cache

---
+++ GPSLocation_Assignment8/FALocationManager.swift
@@ -0,0 +1,53 @@
+//
+//  FALocationManager.swift
+//  GPSLocation_Assignment8
+//
+//  Created by KKNANXX on 6/9/24.
+//
+
+import Foundation
+import CoreLocation
+
+class FALocationManager: NSObject, CLLocationManagerDelegate
+{
+    // MARK:- Variable
+    var locationManager: CLLocationManager!
+    var userLocation: CLLocation?
+    
+    static let sharedInstance: FALocationManager = {
+        let instance = FALocationManager()
+        return instance
+    }()
+    
+    private override init() {
+        super.init()
+    }
+    
+    // Important
+    
+    func setupLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        
+        locationManager.activityType = CLActivityType.otherNavigation
+        locationManager.distanceFilter = 1000.0
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.pausesLocationUpdatesAutomatically = false
+        locationManager.allowsBackgroundLocationUpdates = true
+        
+        if CLLocationManager.locationServicesEnabled() {
+            requestLocationAuthorization()
+        }
+    }
+    
+    
+    func requestLocationAuthorization() {
+        let status = locationManager.authorizationStatus
+        
+        if status == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        }
+        
+        
+    }
+}
